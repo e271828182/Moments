4f16fb0312c499e3909f8031bf9392a5a230f01d
star <e2.718@live.com>
2017-07-10 23:25:00 +0800

    20170710

--- src/main/java/websocket/ReplyHandler.java
+++ src/main/java/websocket/ReplyHandler.java
@@ -4,11 +4,15 @@ import java.util.concurrent.CopyOnWriteArraySet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
 public class ReplyHandler extends AbstractWebSocketHandler {
+
+	
+
 
 	private static final Logger logger = LoggerFactory.getLogger(ReplyHandler.class);
 	
@@ -23,11 +27,21 @@ public class ReplyHandler extends AbstractWebSocketHandler {
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		logger.info("Received message: " + message.getPayload());
-		System.out.println(message.toString());
+		System.out.println(message.getPayload());
+		System.out.println(session.getTextMessageSizeLimit());
+		System.out.println(session.getPrincipal());
 		for (WebSocketSession webSocketSession : webSocketSet) {
-			webSocketSession.sendMessage(new TextMessage("有回复，请查看"));
+			if(!session.equals(webSocketSession)){
+				if(webSocketSession.isOpen())
+					webSocketSession.sendMessage(new TextMessage("有回复，请查看"));				
+			}
 		}
-		
 	}
+
 	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		webSocketSet.remove(session);
+		System.out.println("移除后的大小：："+webSocketSet.size());
+	}
 }
